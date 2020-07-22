38cba5de080104eeaf1fe5a71f11136f29b5c259
johnbickerstaff <john@johnbickerstaff.com>
2020-07-22 15:34:58 -0600

    Adding POJOs.  Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author John Bickerstaff
+ * 22/07/2020
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author John Bickerstaff
+ * 22/07/2020
+ */
+public class Vet extends Person {
+
+
+
+}
