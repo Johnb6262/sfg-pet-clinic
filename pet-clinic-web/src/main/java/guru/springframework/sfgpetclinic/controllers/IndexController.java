d0d836c233abc2e68d9305feb32aa7bd5bf645f1
johnbickerstaff <john@johnbickerstaff.com>
2020-07-27 12:02:12 -0600

    Adding index page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author John Bickerstaff
+ * 27/07/2020
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+}
