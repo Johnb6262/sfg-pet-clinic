3785856809ab9dc57af4fc60afabbbcb6d6266e5
johnbickerstaff <john@johnbickerstaff.com>
2020-08-05 13:04:35 -0600

    added visit, updated owner with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,10 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author John Bickerstaff
  * 23/07/2020
  */
 public class Owner extends Person {
 
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
