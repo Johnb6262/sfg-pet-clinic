ccce5f595c31a162286a509add2716c558d27953
johnbickerstaff <john@johnbickerstaff.com>
2020-08-05 13:10:51 -0600

    added Specialty, added set of specialties to Vet.  Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author John Bickerstaff
+ * 05/08/2020
+ */
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,11 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author John Bickerstaff
  * 23/07/2020
  */
 public class Vet extends Person {
 
+    private Set<Specialty> specialties;
 
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
 
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
