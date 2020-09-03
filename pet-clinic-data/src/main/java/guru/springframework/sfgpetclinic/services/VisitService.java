028a1197cdab6ee55b173d29838d6021f57f2bd1
johnbickerstaff <john@johnbickerstaff.com>
2020-09-03 15:26:01 -0600

    implemented Map Impl for Visit Service.  Closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * @author John Bickerstaff
+ * 03/09/2020
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
